99f7139fdf69f4187406accb1198681d4e1e3d16
Richard Herman <geekyeggo@users.noreply.github.com>
2025-01-09 18:26:54 +0000

    chore: remove generated files to reset casing

--- src/strings/m/icons/folder--filled.g.ts
+++
@@ -1,3 +0,0 @@
-const icon: string =
-	'<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M2 5C2 3.89543 2.89543 3 4 3H8.17157C8.70201 3 9.21071 3.21071 9.58579 3.58579L11.7071 5.70711C11.8946 5.89464 12.149 6 12.4142 6H20C21.1046 6 22 6.89543 22 8V18C22 19.1046 21.1046 20 20 20H4C2.89543 20 2 19.1046 2 18L2 5Z"/></svg>';
-export default icon;
--- src/strings/s/icons/square--filled.g.ts
+++
@@ -1,3 +0,0 @@
-const icon: string =
-	'<svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M2 4C2 2.89543 2.89543 2 4 2H12C13.1046 2 14 2.89543 14 4V12C14 13.1046 13.1046 14 12 14H4C2.89543 14 2 13.1046 2 12V4Z"/></svg>';
-export default icon;
